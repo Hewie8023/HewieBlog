0a37cef52ace87d415f78428ddaf5008616f5490
Hewie8023 <Hewie8023@hotmail.com>
2019-10-13 11:55:47 +0800

    feat:修改secret

--- config/secretKeyConf.js
+++ config/secretKeyConf.js
@@ -1,5 +1,5 @@
 module.exports = {
-    ga='UA-149900822-1',
-    ClientID='1ac633784dc05033dcbf',
-    ClientSecret='5e54f71fc945383488483da46c62fb3b2a83bb4c',
+    ga:'UA-149900822-1',
+    ClientID:'1ac633784dc05033dcbf',
+    ClientSecret:'5e54f71fc945383488483da46c62fb3b2a83bb4c',
 }
