661a59780d0f8199efe39f3ab63e3f95a29a8b1e
Hewie8023 <Hewie8023@hotmail.com>
2019-10-16 00:19:39 +0800

    feat:modify cpp sidebar

--- config/sidebarConf/cpp/index.js
+++ config/sidebarConf/cpp/index.js
@@ -1,5 +1,5 @@
 const utils = require('../../../utils/index.js');
-const children = ['/','cpp1','cpp2','cpp3','cpp4'];
+const children = ['','cpp1','cpp2','cpp3','cpp4'];
 
 module.exports = [
     utils.getSidebar('C++',children),
